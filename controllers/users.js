6860ee8bfb76a0a10f6b88675a5a31eedc12e080
Tomy Robichaud <robichaudtomy@gmail.com>
2019-11-13 11:22:09 -0400

    Async middleware

--- controllers/users.js
+++ controllers/users.js
@@ -1,49 +1,42 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/async');
 const User = require('../models/User');
 
 // @desc    Get all users
 // @route   GET /api/v1/users
 // @access  Public
-exports.getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
 
-    res.status(200).json({
-      success: true,
-      message: 'All users',
-      count: users.length,
-      data: users
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  res.status(200).json({
+    success: true,
+    message: 'All users',
+    count: users.length,
+    data: users
+  });
+});
 
 // @desc    Get a single user
 // @route   GET /api/v1/users
 // @access  Public
-exports.getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
 
-    //Handle non-existant id with correct format
-    if (!user) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with id of ${err.value}`, 404)
-      );
-    }
-    res
-      .status(200)
-      .json({ success: true, message: `User ${req.params.id}`, data: user });
-  } catch (err) {
-    next(err);
+  //Handle non-existant id with correct format
+  if (!user) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${err.value}`, 404)
+    );
   }
-};
+  res
+    .status(200)
+    .json({ success: true, message: `User ${req.params.id}`, data: user });
+});
 
 // @desc    Create a new user
 // @route   POST /api/v1/users
 // @access  Private
-exports.createUser = async (req, res, next) => {
+exports.createUser = asyncHandler(async (req, res, next) => {
   try {
     const user = await User.create(req.body);
 
@@ -55,38 +48,34 @@ exports.createUser = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
 
 // @desc    Create a new user
 // @route   PUT /api/v1/users
 // @access  Private
-exports.updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true
-    });
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true
+  });
 
-    //Handle non-existant id with correct format
-    if (!user) {
-      return next(
-        new ErrorResponse(`Bootcamp not found with id of ${err.value}`, 404)
-      );
-    }
-    res.status(200).json({
-      success: true,
-      message: `Updated user ${req.params.id}`,
-      data: user
-    });
-  } catch (err) {
-    next(err);
+  //Handle non-existant id with correct format
+  if (!user) {
+    return next(
+      new ErrorResponse(`Bootcamp not found with id of ${err.value}`, 404)
+    );
   }
-};
+  res.status(200).json({
+    success: true,
+    message: `Updated user ${req.params.id}`,
+    data: user
+  });
+});
 
 // @desc    Delete a user
 // @route   DELETE /api/v1/users
 // @access  Private
-exports.deleteUser = async (req, res, next) => {
+exports.deleteUser = asyncHandler(async (req, res, next) => {
   try {
     const user = await User.findByIdAndDelete(req.params.id);
 
@@ -103,4 +92,4 @@ exports.deleteUser = async (req, res, next) => {
   } catch (err) {
     next(err);
   }
-};
+});
